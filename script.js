1a725317ea8fcbc540a18ae76d74c15d1725eada
Alitzel <alitrejo650@gmail.com>
2026-08-15 18:08:09 -0600

    cambios colores

--- script.js
+++ script.js
@@ -136,3 +136,51 @@ btn.addEventListener("click", () => {
 
     }
 });
+
+
+
+
+new Swiper(".miGaleria", {
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+
+    /* === NUEVO: Configuración de las flechas === */
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 25
+        }
+    }
+});
+
+const stars = document.querySelector('.stars');
+
+for(let i=0;i<160;i++){
+    const s = document.createElement('div');
+    s.classList.add('star');
+
+    s.style.left = Math.random()*100 + '%';
+    s.style.animationDuration = (Math.random()*8 + 5) + 's';
+    s.style.animationDelay = (Math.random()*5) + 's';
+    s.style.width = (Math.random()*3 + 1) + 'px';
+    s.style.height = s.style.width;
+
+    stars.appendChild(s);
+}
